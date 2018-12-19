5603beb3d9d5d110e2b4204be69d97ea17b02ec2
Christopher Dale <zecheeseking@gmail.com>
2018-12-19 09:47:05 +0000

    First commit, test RTScript

---
+++ rtScript/TestRTScript.js
@@ -0,0 +1,11 @@
+// ====================================================================================================
+//
+// Cloud Code for TestRTScript, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+RTSession.onPacket(100, function(packet)
+{
+    
+});
